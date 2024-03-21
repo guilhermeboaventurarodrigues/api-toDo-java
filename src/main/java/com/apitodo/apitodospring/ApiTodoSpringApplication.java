b5e2069990cb9738c9398edf76cf1e192dbc611e
guilhermeboaventurarodrigues <guilhermeboaventurarodrigues@gmail.com>
2024-03-21 19:14:59 -0300

    first commit

---
+++ src/main/java/com/apitodo/apitodospring/ApiTodoSpringApplication.java
@@ -0,0 +1,13 @@
+package com.apitodo.apitodospring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiTodoSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiTodoSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/apitodo/apitodospring/ApiTodoSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.apitodo.apitodospring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiTodoSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
